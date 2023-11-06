a99e54578fd7415f2d8f5a5a83f3969693f565d8
hennajulkunen <julkunengenna@outlook.com>
2023-11-06 12:08:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/new_version_of_wardpet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.new_version_of_wardpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
